15e6a1e98d534388cfec5ee19ef8f78c831dbb30
natenka <nataliya.samoylenko@gmail.com>
2021-01-26 09:32:48 +0000

    Обновлены ответы 4.x. Добавлены тесты

--- answers/04_data_structures/task_4_7.py
+++ answers/04_data_structures/task_4_7.py
@@ -17,10 +17,5 @@
 
 mac = "AAAA:BBBB:CCCC"
 
-# Вариант решения с помощью функции bin
-bin_mac = bin(int(mac.replace(":", ""), 16))[2:]
-print(bin_mac)
-
-# Вариант решения с форматированием строк
 bin_mac = "{:b}".format(int(mac.replace(":", ""), 16))
 print(bin_mac)
