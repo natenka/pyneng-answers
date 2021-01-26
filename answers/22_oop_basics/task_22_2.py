d296828934e35898460d0b7e5596a8134cd45dab
natenka <nataliya.samoylenko@gmail.com>
2021-01-26 10:20:35 +0000

    Обновлены ответы 22.x

--- answers/22_oop_basics/task_22_2.py
+++ answers/22_oop_basics/task_22_2.py
@@ -5,13 +5,16 @@
 
 Создать класс CiscoTelnet, который подключается по Telnet к оборудованию Cisco.
 
-При создании экземпляра класса, должно создаваться подключение Telnet, а также переход в режим enable.
+При создании экземпляра класса, должно создаваться подключение Telnet, а также
+переход в режим enable.
 Класс должен использовать модуль telnetlib для подключения по Telnet.
 
 У класса CiscoTelnet, кроме __init__, должно быть, как минимум, два метода:
-* _write_line - принимает как аргумент строку и отправляет на оборудование строку преобразованную в байты и добавляет перевод строки в конце.
-  Метод _write_line должен использоваться внутри класса.
-* send_show_command - принимает как аргумент команду show и возвращает вывод полученный с обрудования
+* _write_line - принимает как аргумент строку и отправляет на оборудование строку
+  преобразованную в байты и добавляет перевод строки в конце. Метод _write_line должен
+  использоваться внутри класса.
+* send_show_command - принимает как аргумент команду show и возвращает вывод
+  полученный с обрудования
 
 Параметры метода __init__:
 * ip - IP-адрес
@@ -32,12 +35,19 @@ In [3]: r1_params = {
 In [4]: r1 = CiscoTelnet(**r1_params)
 
 In [5]: r1.send_show_command("sh ip int br")
-Out[5]: 'sh ip int br\r\nInterface                  IP-Address      OK? Method Status                Protocol\r\nEthernet0/0                192.168.100.1   YES NVRAM  up                    up      \r\nEthernet0/1                192.168.200.1   YES NVRAM  up                    up      \r\nEthernet0/2                unassigned      YES manual up                    up      \r\nEthernet0/3                192.168.130.1   YES NVRAM  up                    up      \r\nR1#'
+Out[5]: 'sh ip int br
+Interface                  IP-Address      OK? Method Status                Protocol
+Ethernet0/0                192.168.100.1   YES NVRAM  up                    up      
+Ethernet0/1                192.168.200.1   YES NVRAM  up                    up      
+Ethernet0/2                unassigned      YES manual up                    up      
+Ethernet0/3                192.168.130.1   YES NVRAM  up                    up      
+R1#'
 
 
 Подсказка:
 Метод _write_line нужен для того чтобы можно было сократить строку:
-self.telnet.write(line.encode("ascii") + b"\n")
+self.telnet.write(line.encode("ascii") + b"
+")
 
 до такой:
 self._write_line(line)
