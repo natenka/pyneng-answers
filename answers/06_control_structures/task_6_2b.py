bc8f89ae09af416cc370b2e06861b7cdc2fae5fd
pyneng <pyneng.course@gmail.com>
2022-07-05 05:25:49 +0000

    6.2b

--- answers/06_control_structures/task_6_2b.py
+++ answers/06_control_structures/task_6_2b.py
@@ -39,27 +39,26 @@ else:
     print("unused")
 
 # еще один вариант
-
-while True:
-    ip = input("Введите IP-адрес в формате x.x.x.x: ")
-    octets = ip.split(".")
-    valid_ip = len(octets) == 4
-
-    for i in octets:
-        valid_ip = i.isdigit() and 0 <= int(i) <= 255 and valid_ip
-
-    if valid_ip:
-        break
-    print("Неправильный IP-адрес")
-
-if 1 <= int(octets[0]) <= 223:
-    print("unicast")
-elif 224 <= int(octets[0]) <= 239:
-    print("multicast")
-elif ip == "255.255.255.255":
-    print("local broadcast")
-elif ip == "0.0.0.0":
-    print("unassigned")
-else:
-    print("unused")
-
+#
+# while True:
+#     ip = input("Введите IP-адрес в формате x.x.x.x: ")
+#     octets = ip.split(".")
+#     valid_ip = len(octets) == 4
+#
+#     for i in octets:
+#         valid_ip = i.isdigit() and 0 <= int(i) <= 255 and valid_ip
+#
+#     if valid_ip:
+#         break
+#     print("Неправильный IP-адрес")
+#
+# if 1 <= int(octets[0]) <= 223:
+#     print("unicast")
+# elif 224 <= int(octets[0]) <= 239:
+#     print("multicast")
+# elif ip == "255.255.255.255":
+#     print("local broadcast")
+# elif ip == "0.0.0.0":
+#     print("unassigned")
+# else:
+#     print("unused")
