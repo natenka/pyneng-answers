c63afbf22b88d5d22ebb5d33a98fac98cb53ee3a
Natasha Samoylenko <nataliya.samoylenko@gmail.com>
2024-06-18 11:33:00 +0300

    Update task_15_5.py

--- answers/15_module_re/task_15_5.py
+++ answers/15_module_re/task_15_5.py
@@ -34,12 +34,11 @@ def generate_description_from_cdp(sh_cdp_filename):
         r"(?P<r_dev>\w+)  +(?P<l_intf>\S+ \S+)"
         r"  +\d+  +[\w ]+  +\S+ +(?P<r_intf>\S+ \S+)"
     )
-    description = "description Connected to {} port {}"
     intf_desc_map = {}
     with open(sh_cdp_filename) as f:
         for match in regex.finditer(f.read()):
             r_dev, l_intf, r_intf = match.group("r_dev", "l_intf", "r_intf")
-            intf_desc_map[l_intf] = description.format(r_dev, r_intf)
+            intf_desc_map[l_intf] = f"description Connected to {r_dev} port {r_intf}"
     return intf_desc_map
 
 
